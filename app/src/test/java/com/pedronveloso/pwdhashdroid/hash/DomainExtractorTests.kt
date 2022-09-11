a2bcd5e630107e3e9e87059a2b787f0f5edf4ecc
Pedro Veloso <pedro.n.veloso@gmail.com>
2022-09-11 08:49:32 -0700

    Port over DomainExtractor and HashedPassword classes

---
+++ app/src/test/java/com/pedronveloso/pwdhashdroid/hash/DomainExtractorTests.kt
@@ -0,0 +1,57 @@
+package com.pedronveloso.pwdhashdroid.hash
+
+import com.pedronveloso.pwdhashdroid.hash.DomainExtractor.extractDomain
+import junit.framework.TestCase.assertEquals
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class DomainExtractorTest {
+
+    lateinit var testSamples: HashMap<String, String>
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        testSamples = HashMap()
+        testSamples["example.com"] = "example.com"
+        testSamples["http://example.com"] =
+            "example.com"
+        testSamples["http://example.com/aPath/test.html"] =
+            "example.com"
+        testSamples["http://www.example.com"] =
+            "example.com"
+        testSamples["https://www.example.com"] =
+            "example.com"
+        testSamples["http://www.example.com/aPath/test.html"] =
+            "example.com"
+        testSamples["http://login.test.example.com"] =
+            "example.com"
+        testSamples["http://example.co.uk"] =
+            "example.co.uk"
+        testSamples["http://login.example.co.uk"] =
+            "example.co.uk"
+        testSamples["https://login.example.co.uk/test.htm"] =
+            "example.co.uk"
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testExtractDomain() {
+        testSamples.forEach { (k, v) ->
+            assertEquals(v, extractDomain(k))
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testExtractDomainWithEmptyStringInput() {
+        Assert.assertEquals("", extractDomain(""))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    @Throws(Exception::class)
+    fun testExtractDomainWithNullInput() {
+        extractDomain(null)
+    }
+}
---
+++ app/src/test/java/com/pedronveloso/pwdhashdroid/hash/HashedPasswordTest.kt
@@ -0,0 +1,88 @@
+package com.pedronveloso.pwdhashdroid.hash
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.pedronveloso.pwdhashdroid.hash.HashedPassword.Companion.create
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(AndroidJUnit4::class)
+class HashedPasswordTest {
+
+    @Test
+    fun testToString() {
+        val hashedPassword = create(
+            "my53cret#",
+            "example.com"
+        )
+        Assert.assertEquals("Bu6aSm+Zcsf", hashedPassword.toString())
+    }
+
+    @Test
+    fun testToStringWithNonAsciiChars() {
+        val hashedPassword = create(
+            "mü53crét#",
+            "example.com"
+        )
+        Assert.assertEquals("r9qeSjv+lwJ", hashedPassword.toString())
+    }
+
+    @Test
+    fun testToStringWithNonLatin1Chars() {
+        val hashedPassword = create(
+            "中文العربي",
+            "example.com"
+        )
+        Assert.assertEquals("AwMz3+BdMT", hashedPassword.toString())
+    }
+
+    @Test
+    fun testToStringWithoutNonAlphanumeric() {
+        val hashedPassword = create(
+            "my53cret",
+            "example.com"
+        )
+        Assert.assertEquals("CIUD4SCSgh", hashedPassword.toString())
+    }
+
+    @Test
+    fun testToStringWithShortSecret() {
+        val hashedPassword = create(
+            "ab",
+            "example.com"
+        )
+        Assert.assertEquals("0IKv", hashedPassword.toString())
+    }
+
+    @Test
+    fun testToStringWithShortestSecret() {
+        val hashedPassword = create(
+            "a",
+            "example.com"
+        )
+        Assert.assertEquals("9FBo", hashedPassword.toString())
+    }
+
+    @Test
+    fun testToStringWithLongSecret() {
+        val hashedPassword = create(
+            "abcdefghijklmnopqrstuvwxyz0123456789=", "example.com"
+        )
+        val result = hashedPassword.toString()
+
+        // The original algorithm appends NULL bytes at the beginning.
+        // Those bytes should not be part of the output.
+        // "\0\0\0\0XO3u58jVa1nd+8qd08SDIQ"
+        Assert.assertEquals("XO3u58jVa1nd+8qd08SDIQ", result)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testToStringWithEmptySecret() {
+        val hashedPassword = create(
+            "",
+            "example.com"
+        )
+        hashedPassword.toString()
+    }
+}
