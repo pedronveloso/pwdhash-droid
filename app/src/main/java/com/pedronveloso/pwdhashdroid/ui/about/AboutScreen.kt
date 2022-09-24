62de12642125bb590a18fd81b86d661789c14709
Pedro Veloso <pedro.n.veloso@gmail.com>
2022-09-23 17:59:01 -0700

    Display App name and version on About Screen

--- app/src/main/java/com/pedronveloso/pwdhashdroid/ui/about/AboutScreen.kt
+++ app/src/main/java/com/pedronveloso/pwdhashdroid/ui/about/AboutScreen.kt
@@ -2,10 +2,11 @@
 
 package com.pedronveloso.pwdhashdroid.ui.about
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -13,8 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pedronveloso.pwdhashdroid.BuildConfig
 import com.pedronveloso.pwdhashdroid.R
+
 
 @Composable
 fun AboutScreen(onNavBackPressed: () -> Unit) {
@@ -41,11 +48,46 @@ fun AboutScreen(onNavBackPressed: () -> Unit) {
             Column(
                 modifier = Modifier
                     .padding(it)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "About screen contents here!")
+                val context = LocalContext.current
+                val screenContents = buildScreenContents(context)
+                AboutScreenContent(screenContents)
             }
         })
+}
+
+private fun buildScreenContents(context: Context): AboutScreenContents {
+    val applicationInfo: ApplicationInfo = context.applicationInfo
+    val stringId = applicationInfo.labelRes
+    val appName =
+        if (stringId == 0) applicationInfo.nonLocalizedLabel.toString() else context.getString(
+            stringId
+        )
+    val appVersion = BuildConfig.VERSION_NAME
+    val websiteUrl = "https://pedronveloso.com"
+    val websiteLabel = "pedronveloso.com"
+    return AboutScreenContents(
+        appName = appName,
+        appVersion = appVersion,
+        websiteUrl = websiteUrl,
+        websiteLabel = websiteLabel
+    )
+}
+
+private class AboutScreenContents(
+    val appName: String,
+    val appVersion: String,
+    val websiteUrl: String,
+    websiteLabel: String
+)
+
+@Composable
+private fun AboutScreenContent(screenContents: AboutScreenContents) {
+    Spacer(modifier = Modifier.size(16.dp))
+    Text(text = screenContents.appName, fontSize = 32.sp, fontWeight = FontWeight.Normal)
+    Text(text = screenContents.appVersion, fontSize = 24.sp, fontWeight = FontWeight.Light)
 }
